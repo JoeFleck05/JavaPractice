7b8085fd3d9e6f2a3c4edb3d60c9fc813a34c614
JoeFleck05 <joefleck05@gmail.com>
2017-06-01 22:55:47 -0400

    Reg String start

--- HackerRankExercises/src/HackerRankExercises/StringsIntroduction.java
+++ HackerRankExercises/src/HackerRankExercises/StringsIntroduction.java
@@ -1,10 +1,35 @@
 package HackerRankExercises;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class StringsIntroduction {
 	
 	public static String stringIntroduction (String a, String b) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add(a);
+		list.add(b);
 		String returnString = null;
+		int total = 0;
 		
+		total = a.length() + b.length();
+		
+		Collections.sort(list);
+		
+		String stringFirst = list.get(0);
+		String stringLast = list.get(list.size() - 1);
+		String stringAnswer = "";
+		String stringNo = "";
+		
+		if(stringFirst.compareTo(stringLast) > 0) {
+			stringAnswer = "Yes";			
+		} else {
+			stringAnswer = "No";
+		}
+		
+		System.out.println(total);
+		System.out.println(stringAnswer);
+		System.out.println(a.substring(0));
 		return returnString;
 	}
 
