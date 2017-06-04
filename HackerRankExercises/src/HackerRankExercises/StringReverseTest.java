0339649baad5c5d10a64d177f560e1f476a312cd
JoeFleck05 <joefleck05@gmail.com>
2017-06-04 15:19:44 -0400

    add more tests

--- HackerRankExercises/src/HackerRankExercises/StringReverseTest.java
+++ HackerRankExercises/src/HackerRankExercises/StringReverseTest.java
@@ -8,13 +8,25 @@ public class StringReverseTest {
     
     StringReverse javaStringReverse = new StringReverse();
     
-    private final String EXPECTED_ANSWER = "Yes";
+    private final String EXPECT_YES = "Yes";
+    private final String EXPECT_NO = "No";
 
     @Test
-    public void test() {
+    public void testOne() {
         String A = "madam";
-        
-        assertEquals(EXPECTED_ANSWER, StringReverse.reverseString(A));
+        assertEquals(EXPECT_YES, StringReverse.reverseString(A));
+    }
+    
+    @Test
+    public void testTwo() {
+    	String A = "joemomma";
+    	assertEquals(EXPECT_NO, StringReverse.reverseString(A));
+    }
+    
+    @Test
+    public void testThree() {
+    	String A = "mom";
+    	assertEquals(EXPECT_YES, StringReverse.reverseString(A));
     }
 
 }
